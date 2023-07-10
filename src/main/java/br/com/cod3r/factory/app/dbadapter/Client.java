1aa49b67339197986f493fb432cf55567e4889a8
uluizeduardo <luiz.analistadesistemas@gmail.com>
2023-07-09 22:46:39 -0300

    Refactored the code and added the Factory Metchod pattern (Simple Factory)

--- src/main/java/br/com/cod3r/factory/app/dbadapter/Client.java
+++ src/main/java/br/com/cod3r/factory/app/dbadapter/Client.java
@@ -1,8 +1,20 @@
 package br.com.cod3r.factory.app.dbadapter;
 
+import br.com.cod3r.factory.app.dbadapter.db.DB;
+import br.com.cod3r.factory.app.dbadapter.factory.DBFactory;
+
+import java.util.Optional;
+
 public class Client {
 
 	public static void main(String[] args) {
-
+		Optional<DB> dbFactory = new DBFactory().getDatabase("oracle");
+		dbFactory.ifPresentOrElse(db -> {
+					db.query("SELECT * FROM users");
+					db.update("INSERT INTO users VALUES ('User', 25)");
+				},
+				() -> {
+					System.out.println("DatabaseProvider not found");
+				});
 	}
 }
--- src/main/java/br/com/cod3r/factory/app/dbadapter/factory/DBFactory.java
+++ src/main/java/br/com/cod3r/factory/app/dbadapter/factory/DBFactory.java
@@ -1,7 +1,18 @@
 package br.com.cod3r.factory.app.dbadapter.factory;
 
 import br.com.cod3r.factory.app.dbadapter.db.DB;
+import br.com.cod3r.factory.app.dbadapter.db.OracleDB;
+import br.com.cod3r.factory.app.dbadapter.db.PostgresDB;
 
-public interface DBFactory {
-	DB getDatabase();
+import java.util.Optional;
+
+public class DBFactory {
+	public Optional<DB> getDatabase(String databaseProvider){
+		if("ORACLE".equals(databaseProvider.toUpperCase())){
+			return Optional.of(new OracleDB());
+		} else if ("POSTGRES".equals(databaseProvider.toUpperCase())){
+			return Optional.of(new PostgresDB());
+		}
+		return Optional.empty();
+	}
 }
